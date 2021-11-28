7e0dbfd9383f4e61a977a929dac090d648b1a1ed
srgey kolychev <sitisk8ll8@gmail.com>
2021-11-29 00:11:46 +0300

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,16 +34,18 @@
 // }
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
-    
+     
     let countOfGoods = goods.length;
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
     }
-      
+    data = {client: {name, phone}, order: {address, sum}, goods};
+    data.client = name + phone;
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-    data.client = 'Иван';
+    data.order.sum = sum;
+    //data.order.address = address;
+    //data.order.sum = name + phone + address + goods + sum;
+    //data.client = 'Иван';
     let jsonData = JSON.stringify(data);
-    data = {client: {name, phone},order: {address, sum}, goods: [{title, count}]};
     return jsonData;
 }
