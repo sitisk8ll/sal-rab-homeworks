29cad0f5813c89ef6beb72e6e6d12f3f96efd931
srgey kolychev <sitisk8ll8@gmail.com>
2021-12-01 02:40:30 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -31,21 +31,22 @@
 //         }
 //      ]
 //    }
-// }
+// } 
+// Внутри цикла вы должны добавить не только заголовок продукта, а сформировать объект из свойств title и count. Посмотрите,
+// у вас есть обращение к названию продукта goods[i].title, это значение вы должны сделать
+// в свойстве title, аналогично и для свойства count. В массив пушить этот формируемый объект.
+
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
-     
+
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        goods[i] = {title:goods[i].title, count:goods[i].count};
+        data.goods.push(goods[i]);
     }
-    data = {client: {name, phone}, order: {address, sum}, goods};
-    data.client = name + phone;
-    data.order.address = address;
+    data.client = name + ' ' + phone;
+    data.order.address = 'ул.' + ' ' + address.street +',' + ' ' + 'дом' + ' ' + address.house + ',' + ' ' + address.entrance +' '+ 'подъезд' + ',' + ' ' + address.floor +' '+ 'этаж'+ ','+' '+ 'кв' + ' ' + address.flat; 
     data.order.sum = sum;
-    //data.order.address = address;
-    //data.order.sum = name + phone + address + goods + sum;
-    //data.client = 'Иван';
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
